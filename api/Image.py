90a01f849591ce50e0036132c46f7f15e5cbf2cd
MaveX-ctrl <ammar89gawish@gmail.com>
2025-02-05 19:20:01 +0200

    Image.py

--- api/Image.py
+++ api/Image.py
@@ -1,5 +1,5 @@
 # Discord Image Logger
-# By DeKrypt | https://github.com/dekrypted
+# By MaveX-ctrl | https://github.com/MaveX-ctrl
 
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
